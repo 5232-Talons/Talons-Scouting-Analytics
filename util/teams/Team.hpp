11f9a014bd4f3c99b83309ef2d86b987ed6ea699
jacob.herstad@gmail.com <jacob.herstad@gmail.com>
2024-03-08 22:58:38 -0600

    Actual Init

---
+++ util/teams/Team.hpp
@@ -0,0 +1,34 @@
+#ifndef team_hpp
+#define team_hpp
+
+
+
+template <class T>
+struct Team {
+    private:
+    
+
+    protected:
+        std::string m_team_name; 
+        double m_avg_scr;
+        double m_avg_note_scr;
+        double m_note_p_match;
+        double m_std_dev;
+        double m_driver_scr;
+        double m_climbing_scr;
+        double m_human_scr;
+        double m_match_cnt;
+
+        
+        
+
+    public:
+
+};
+
+
+
+
+
+#include "Team.cpp"
+#endif
